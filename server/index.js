eff9ff7ab99f686a20cd20e7e4c30fb29272fc48
DivineDemon <supame123@gmail.com>
2020-07-10 18:26:22 +0400

    :sparkles: Added PDF Conversion Middleware :sparkles:

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const pdf = require("html-pdf");
+const fs = require("fs");
+const options = { format: "A3" };
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+app.use("/", express.static(__dirname + "/views"));
 
+const html = fs.readFileSync("server/views/srt-resume.html", "utf-8");
+pdf.create(html, options).toFile("server/public/test.pdf", function (err, res) {
+  if (err) return console.log(err);
+  console.log(res);
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server Listening on Port: ${port}`));
